6e888e2b9168495c4c5cb24c05e0f861d40ac62f
Edgar-A23 <edgar.rodriguezp@uanl.edu.mx>
2025-04-27 22:14:52 -0600

    Update diffie_hellman.py

--- Metodos_Criptograficos/diffie_hellman.py
+++ Metodos_Criptograficos/diffie_hellman.py
@@ -8,26 +8,27 @@ def procesamiento(p, g, x):
     return y
 
 
-os.system("cls")
+if __name__ == "__main__":
+    os.system("cls")
 
-p = int(input("Ingrese el número primo público"
-              " para el método de encriptación: "))
+    p = int(input("Ingrese el número primo público"
+                  " para el método de encriptación: "))
 
-g = int(input("Ingrese un número a cifrar: "))
+    g = int(input("Ingrese un número a cifrar: "))
 
-# Solicitamos la clave privada de los usuarios
-a = int(input("Ingrese la clave privada que usará el usuario 1: "))
-b = int(input("Ingrese la clave privada que usará el usuario 2: "))
+    # Solicitamos la clave privada de los usuarios
+    a = int(input("Ingrese la clave privada que usará el usuario 1: "))
+    b = int(input("Ingrese la clave privada que usará el usuario 2: "))
 
-# Obtenemos las claves públicas que obtendrían los dos usuarios
-A = procesamiento(p, g, a)
-B = procesamiento(p, g, b)
+    # Obtenemos las claves públicas que obtendrían los dos usuarios
+    A = procesamiento(p, g, a)
+    B = procesamiento(p, g, b)
 
-# Obtenemos el mensaje cifrado
-K1 = procesamiento(p, B, a)
-K2 = procesamiento(p, A, b)
+    # Obtenemos el mensaje cifrado
+    K1 = procesamiento(p, B, a)
+    K2 = procesamiento(p, A, b)
 
-if K1 == K2:
-    print(f"La clave privada compartida es: {K1}")
-else:
-    print("Error en el cifrado")
+    if K1 == K2:
+        print(f"La clave privada compartida es: {K1}")
+    else:
+        print("Error en el cifrado")
